13c245c708a9c12619ca24fbff75b0d61136f864
ziyagasimov <ziyagasimov0@gmail.com>
2025-12-11 18:04:32 +0000

    t3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that provides a function to return the floor of a float."""
+
+
+def to_str(n: float) -> str:
+    """Calculates the floor of a float."""
+    return str(n)
