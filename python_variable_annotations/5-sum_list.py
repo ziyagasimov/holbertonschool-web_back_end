2ad63315c257f40cd54f1efd06a966160c809523
ziyagasimov <ziyagasimov0@gmail.com>
2025-12-12 10:29:53 +0000

    t5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that provides a function to return the floor of a float."""
+
+
+def sum_list(input_list: float) -> float:
+    """Calculates the floor of a float."""
+    return input_list
