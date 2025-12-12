6053a2529b4da540ecdba7bf6ef8806fad3a9cf3
ziyagasimov <ziyagasimov0@gmail.com>
2025-12-12 13:00:41 +0000

    t5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+
+
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
     """Takes a list of floats as input and returns their sum as a float."""
-
-
     return sum(input_list)
