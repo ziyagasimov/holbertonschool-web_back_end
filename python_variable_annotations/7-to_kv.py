34ec0598ee6a3b53ce3a7906b873c6abdcc2b8cf
ziyagasimov <ziyagasimov0@gmail.com>
2025-12-14 16:21:56 +0000

    t7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Type-annotated function to_kv module."""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Takes a string k and an int or float v as arguments and returns a tuple.
+    The first element of the tuple is the string k.
+    The second element is the square of the int/float v and should be annotated as a float."""
+
+
+    return (k, float(v**2))
