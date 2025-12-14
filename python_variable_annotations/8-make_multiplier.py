f55f9a4394ef36f6e3fe1815def699121c5f8f5c
ziyagasimov <ziyagasimov0@gmail.com>
2025-12-14 16:30:23 +0000

    t8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""Type-annotated function to_kv module."""
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def multiplier_function(n: float) -> float:
+        """Takes a string k and an int or float v as arguments"""
         return n * multiplier
     return multiplier_function
