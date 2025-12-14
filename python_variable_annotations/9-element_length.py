2bd49380a7246b67cccf1e0f885b9e3c8494f46a
ziyagasimov <ziyagasimov0@gmail.com>
2025-12-14 16:45:59 +0000

    t9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Type-annotated function to_kv module."""
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Takes a string k and an int or float v as arguments"""
     return [(i, len(i)) for i in lst]
