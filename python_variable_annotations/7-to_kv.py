2c182094e3b6680d2190e273bc1256d5a7fb25b1
ziyagasimov <ziyagasimov0@gmail.com>
2025-12-14 16:25:03 +0000

    t7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Takes a string k and an int or float v as arguments and returns a tuple."""
+    """Takes a string k and an int or float v as arguments"""
 
 
     return (k, float(v**2))
