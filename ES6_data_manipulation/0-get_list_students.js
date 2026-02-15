539b0694f431a7427f770d0fe8df27598d2cafba
ziyagasimov <ziyagasimov0@gmail.com>
2026-02-15 17:12:40 +0000

    to

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * Tələbələrin siyahısını qaytaran funksiya.
+ * @returns {Array} Tələbə obyektlərindən ibarət massiv.
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
