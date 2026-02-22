bf0e1d1acdd3542eb27fa323803886806b0c5400
ziyagasimov <ziyagasimov0@gmail.com>
2026-02-22 19:48:19 +0000

    t0

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    /* * For this basic task, we just need to return the instance.
+     * We can immediately resolve it or leave it pending.
+     */
+    resolve();
+  });
+}
