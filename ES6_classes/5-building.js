a915f5b415e6c8eac683c21a687b58128cc09c9b
ziyagasimov <ziyagasimov0@gmail.com>
2026-04-01 17:47:07 +0400

    hbtn

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    // Abstract method enforcement
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+}
